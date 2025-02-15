28e1e9ba44d69d741f35c99cf64929f68aa1238e
Chris <neiltafurdev@gmail.com>
2025-02-15 17:02:04 -0500

    crud categoria falta eliminar y toast

--- src/app/views/categorias/categorias.component.ts
+++ src/app/views/categorias/categorias.component.ts
@@ -48,6 +48,7 @@ import { MasterService } from '../../_service/master.service';
 export class CategoriasComponent {
 
   isDataLoaded: boolean = false; 
+  public visible = false;
   categoriaForm: FormGroup;
   categoriaLista: Categoria[]=[];
   dtOptions: Config = {};
@@ -56,6 +57,7 @@ export class CategoriasComponent {
 
   @ViewChildren(ToasterComponent) viewChildren!: QueryList<ToasterComponent>;
   @ViewChild('cerrarModal') cerrarModal!: ElementRef;
+  @ViewChild('categoriasModal') categoriasModal!: ElementRef;
   @ViewChild('modalTitulo', { static: false }) modalTitulo!: ElementRef;
   @ViewChild('toaster') toaster!: ToasterComponent;
   @ViewChild(DataTableDirective, { static: false }) dtElement!: DataTableDirective;
@@ -78,54 +80,95 @@ export class CategoriasComponent {
     this.id = this.activatedRoute.snapshot.paramMap.get('id')!;
   }
 
+  // ngOnInit(): void {
+  //   this.getCategorias();
+  //   this.dtOptions = {
+  //     pagingType: 'full',
+  //     pageLength: 5,
+  //     processing: true
+  //   };
+  // }
   ngOnInit(): void {
     this.getCategorias();
     this.dtOptions = {
       pagingType: 'full',
       pageLength: 5,
-      processing: true
-    };
-    
-
-  }
-
-  // getCategorias(): void {
-  //   this.service.getCategorias().subscribe({
-  //     next: (data: Categoria[]) => {
-  //       console.log('Categorias recibidas:', data);
-  //       this.categoriaLista = data;
-  //       this.dtTrigger.next(null);  // Esto asegura que DataTables cargue correctamente los datos
-  //     },
-  //     error: (err) => {
-  //       console.error('Error al cargar categorías:', err);
-  //     }
-  //   });
-  // }
-  getCategorias(): void {
-    this.service.getCategorias().subscribe({
-      next: (data: Categoria[]) => {
-        this.categoriaLista = data;
-        this.isDataLoaded = true;  // Cambia a true cuando los datos se cargan
-        this.dtTrigger.next(null);
+      processing: true,
+      ajax: (dataTablesParameters: any, callback) => {
+        this.service.getCategorias().subscribe((data: Categoria[]) => {
+          callback({
+            recordsTotal: data.length,
+            recordsFiltered: data.length,
+            data: data
+          });
+        });
       },
-      error: (err) => console.error('Error al cargar categorías:', err)
-    });
+      columns: [
+        { title: 'Nombre', data: 'nombre' },
+        { title: 'Descripción', data: 'descripcion' },
+        { title: 'Fecha de Creación', data: 'fechaCreacion' },
+        { title: 'Acciones', data: null }
+      ],
+      rowCallback: (row: Node, data: any, index: number) => {
+        const self = this;
+      
+        $('td:eq(3)', row).html(`
+          <button class="btn btn-sm btn-warning edit-btn me-2">
+            <i class="fas fa-pencil-alt"></i>
+          </button>
+          <button class="btn btn-sm btn-danger delete-btn">
+            <i class="fas fa-trash-alt"></i>
+          </button>
+        `);
+      
+        $(row).find('.edit-btn').on('click', () => {
+          console.log("Editar 1");
+          this.abrirModalEdicion(data)
+          
+        });
+        
+      
+        $(row).find('.delete-btn').on('click', () => {
+         // self.eliminarCategoria((data as Categoria)._id);  // Convertimos a Categoria
+         console.log("Eliminar 1");
+         
+        });
+      
+        return row;
+      }
+      
+    };
+
+    
+    
+    // this.dtOptions = {
+    //   pagingType: 'full',
+    //   pageLength: 5,
+    //   processing: true,
+    //   ajax: (dataTablesParameters: any, callback) => {
+    //     this.service.getCategorias().subscribe((data: Categoria[]) => {
+    //       callback({
+    //         recordsTotal: data.length,
+    //         recordsFiltered: data.length,
+    //         data: data
+    //       });
+    //     });
+    //   },
+    //   columns: [
+    //     { title: 'Nombre', data: 'nombre' },
+    //     { title: 'Descripción', data: 'descripcion' },
+    //     { title: 'Fecha de Creación', data: 'fechaCreacion' },
+    //     { title: 'Acciones', data: null }  // Esto lo manejas con rowCallback
+    //   ]
+    // };
   }
-  
-
-  reloadComponent() {
-    this.router.navigateByUrl('/productos', { skipLocationChange: true }).then(() => {
-      this.router.navigate(['/categorias']);
-    });
-  }
-
-
- 
 
  
   abrirModalEdicion(categoria: Categoria): void {
+    this.visible = !this.visible;
     // Cambia el título del modal
     this.modalTitulo.nativeElement.textContent = 'Editar categoría';
+
     
     // Asigna el ID y carga los valores en el formulario
     this.id = categoria._id!;
@@ -141,7 +184,57 @@ export class CategoriasComponent {
     this.id = null!;
     this.categoriaForm.reset();
   }
+
+  toggleLiveDemo() {
+    this.visible = !this.visible;
+    this.modalTitulo.nativeElement.textContent = 'Editar categoría';
+  }
+  handleLiveDemoChange(event: any) {
+    this.visible = event;
+  }
+
+
   
+  // getCategorias(): void {
+  //   this.service.getCategorias().subscribe({
+  //     next: (data: Categoria[]) => {
+  //       this.categoriaLista = data;
+  //       this.isDataLoaded = true;  // Cambia a true cuando los datos se cargan
+  //       this.dtTrigger.next(null);
+  //     },
+  //     error: (err) => console.error('Error al cargar categorías:', err)
+  //   });
+  // }
+  getCategorias(): void {
+    this.service.getCategorias().subscribe({
+      next: (data: Categoria[]) => {
+        this.categoriaLista = data;
+        this.isDataLoaded = true;
+        this.dtTrigger.next(null);  // Renderiza la tabla
+      },
+      error: (err) => console.error('Error al cargar categorías:', err)
+    });
+  }
+
+  // recargarTabla(): void {
+  //   this.service.getCategorias().subscribe((data: Categoria[]) => {
+  //     const tabla = $('#miTabla').DataTable(); // Asegúrate de poner el ID correcto de tu tabla
+  //     tabla.clear();               // Limpia los datos actuales
+  //     tabla.rows.add(data);        // Agrega los nuevos datos
+  //     tabla.draw();                // Redibuja la tabla
+  //   });
+  // }
+  recargarTabla(): void {
+    const table = $('#categoriasTable').DataTable();
+    this.service.getCategorias().subscribe({
+      next: (data: Categoria[]) => {
+        table.clear();
+        table.rows.add(data);
+        table.draw();
+      },
+      error: (err) => console.error('Error al recargar datos:', err)
+    });
+  }
 
   
   saveCategoria(): void {
@@ -154,11 +247,11 @@ export class CategoriasComponent {
       // Editamos una categoría existente
       this.service.updateCategoria(this.id, categoria).subscribe(
         () => {
-          this.router.navigate(['/'], { replaceUrl: true });
+          //this.router.navigate(['/'], { replaceUrl: true });
           this.cerrarModal.nativeElement.click(); // Cierra el modal
           
           
-          //this.reloadComponent();
+          this.recargarTabla();
           
           
         },
@@ -173,7 +266,7 @@ export class CategoriasComponent {
       this.service.saveCategoria(categoria).subscribe(
         () => {
           this.cerrarModal.nativeElement.click(); // Cierra el modal
-          // this.reloadComponent();
+          this.recargarTabla();
         },
         (error) => {
           console.error(error);
